492ac26a4f1e726aa23a4ebebfd10a510a016db0
Trej Gun <trejgun@gmail.com>
2016-04-27 13:04:55 +0300

    code: addPaginationHeaders moved to common

--- source/response.js
+++ source/response.js
@@ -1,8 +1,6 @@
 "use strict";
 
 import debug from "debug";
-import querystring from "querystring";
-import {getServerUrl} from "./misc";
 import {makeError} from "./error";
 import {translate} from "abl-lang";
 
@@ -49,35 +47,6 @@ export function sendError(error, request, response, next) {
 		}
 	}
 	return send(error);
-}
-
-export function addPaginationHeaders(request, response, count) {
-	const last = Math.ceil(count / request.query.pageSize);
-	const url = `${getServerUrl()}${request.route.path}?`;
-
-	response.set("X-First-Page-Url", url + querystring.stringify({
-			pageSize: request.query.pageSize,
-			page: 0
-		}));
-
-	if (request.query.page !== 0) {
-		response.set("X-Prev-Page-Url", url + querystring.stringify({
-				pageSize: request.query.pageSize,
-				page: request.query.page - 1
-			}));
-	}
-
-	if (request.query.page !== last) {
-		response.set("X-Next-Page-Url", url + querystring.stringify({
-				pageSize: request.query.pageSize,
-				page: request.query.page + 1
-			}));
-	}
-
-	response.set("X-Last-Page-Url", url + querystring.stringify({
-			pageSize: request.query.pageSize,
-			page: last
-		}));
 }
 
 export function wrapJSON(method) {
